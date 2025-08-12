b1180f55c0d588215f88a57804f4e753c48a061f
Mynul Alam <mynulalam27@gmail.com>
2025-08-12 21:41:12 +0600

    money Management

--- lib/money_managment.dart
+++ lib/money_managment.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MoneyManagment extends StatelessWidget {
+class MoneyManagment extends StatefulWidget {
   const MoneyManagment({super.key});
 
+  @override
+  State<MoneyManagment> createState() => _MoneyManagmentState();
+}
+
+class _MoneyManagmentState extends State<MoneyManagment> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();

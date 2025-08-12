b500e2e1c39d68c151ef60f57fc866b38cfef4a2
Mynul Alam <mynulalam27@gmail.com>
2025-08-12 22:38:05 +0600

    c

--- lib/money_managment.dart
+++ lib/money_managment.dart
@@ -8,6 +8,7 @@ class MoneyManagment extends StatefulWidget {
 }
 
 class _MoneyManagmentState extends State<MoneyManagment> {
+  late TabController _tabController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +18,35 @@ class _MoneyManagmentState extends State<MoneyManagment> {
           "Money Management",
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: "Earning", icon: Icon(Icons.arrow_upward)),
+            Tab(text: "Expense", icon: Icon(Icons.arrow_downward)),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Card(
+                color: Colors.green,
+                child: Column(children: [Text("Earing"), Text("500 TK")]),
+              ),
+              Card(
+                color: Colors.red,
+                child: Column(children: [Text("Earing"), Text("500 TK")]),
+              ),
+              Card(
+                color: Colors.blue,
+                child: Column(children: [Text("Earing"), Text("500 TK")]),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          TabBarView(controller: _tabController, children: []),
+        ],
       ),
     );
   }

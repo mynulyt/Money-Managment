c3c6b78a9e46440c14c32d0218f2b57503055d78
Mynul Alam <mynulalam27@gmail.com>
2025-08-12 21:40:43 +0600

    money managment

---
+++ lib/money_managment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoneyManagment extends StatelessWidget {
+  const MoneyManagment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

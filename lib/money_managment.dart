708b5171860adec620739b6916866de575a2141e
Mynul Alam <mynulalam27@gmail.com>
2025-08-13 00:03:40 +0600

    card

--- lib/money_managment.dart
+++ lib/money_managment.dart
@@ -35,35 +35,58 @@ class _MoneyManagmentState extends State<MoneyManagment>
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Card(
-                color: Colors.green,
-                child: Column(children: [Text("Earing"), Text("500 TK")]),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                buildCardSummary(),
+                buildCardSummary(),
+                buildCardSummary(),
+              ],
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [buldCard(), buldCard()],
               ),
-              Card(
-                color: Colors.red,
-                child: Column(children: [Text("Earing"), Text("500 TK")]),
-              ),
-              Card(
-                color: Colors.blue,
-                child: Column(children: [Text("Earing"), Text("500 TK")]),
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          TabBarView(
-            controller: _tabController,
-            children: [buldCard(), buldCard()],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  ListView buldCard() {
+  Widget buildCardSummary() {
+    return Expanded(
+      child: Card(
+        color: Colors.green,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Text(
+                "Earing",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              Text(
+                "500 TK",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buldCard() {
     return ListView.builder(
       itemCount: 10,
       itemBuilder: (context, index) {
